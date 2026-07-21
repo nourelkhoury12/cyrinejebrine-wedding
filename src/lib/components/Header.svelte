949d92a73909a60cd5a19a88505a550d48f86f2a
nourelkhoury12 <nourkhoury201@gmail.com>
2026-07-21 12:40:47 +0300

    add fade fly effect

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,6 +3,7 @@
   const groom = "Teddy";
   import OrhcidLeft from "$lib/assets/orchid-left.png"
   import OrhcidRight from "$lib/assets/orchid-rigth.png"
+  import { fade, fly } from 'svelte/transition';
   import Image from "$lib/assets/image.png"
   import Rose from "$lib/assets/rose.png"
 </script>
@@ -11,13 +12,13 @@
 
     <div class="card-paper"></div>
 
-        <img src={OrhcidLeft}  alt="" class="floral-decoration top-orchid"/>
-        <img src={OrhcidRight}  alt="" class="floral-decoration top-orchid-left"/>
+        <img src={OrhcidLeft} in:fly={{ y: -30, duration: 1000 }} alt="" class="floral-decoration top-orchid"/>
+        <img src={OrhcidRight} in:fly={{ y: -30, duration: 1000 }} alt="" class="floral-decoration top-orchid-left"/>
 
         <div class="card-header">
-            <p class="card-save-date">The Wedding Of</p>
-            <span class="beginning">Cyrine & Jebrine</span>
-            <p class="card-save-date">SAVE THE DATE</p>
+            <p class="card-save-date" in:fade={{ duration: 1000 }}>The Wedding Of</p>
+            <span class="beginning" in:fade={{ duration: 1000 }}>Cyrine & Jebrine</span>
+            <p class="card-save-date" in:fade={{ duration: 1000 }}>SAVE THE DATE</p>
         
         </div>
     </div>
@@ -71,12 +72,9 @@
     
 }
 
-
-
 .card-header {
     position: relative;
     z-index: 2;
-
     margin-top: 28%;
     text-align: center;
 }
@@ -85,9 +83,7 @@
 
 
 .card-save-date {
-
     margin-top: 1.2rem;
-
     font-family: "Cinzel", serif;
     font-size: clamp(.8rem,1vw,1rem);
     letter-spacing: .35em;
