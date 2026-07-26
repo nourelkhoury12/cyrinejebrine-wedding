8854f8873a1fcd658ab4ec96ed32380d0b7fb5ab
nourelkhoury12 <nourkhoury201@gmail.com>
2026-07-26 19:24:23 +0300

    window default scrool refresh

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,29 @@
 <script lang="ts">
-    import BgImage from "$lib/assets/backgd.png";
+    import { onMount } from "svelte";
+
     import CountDown from "$lib/components/CountDown.svelte";
-  import Footer from "$lib/components/Footer.svelte";
-	import Header from "$lib/components/Header.svelte";
+    import Footer from "$lib/components/Footer.svelte";
+    import Header from "$lib/components/Header.svelte";
     import MarriageList from "$lib/components/MarriageList.svelte";
     import RSVP from "$lib/components/RSVP.svelte";
-  	import Venue from "$lib/components/Venue.svelte";
-</script>
+    import Venue from "$lib/components/Venue.svelte";
 
+    onMount(() => {
+        if ("scrollRestoration" in history) {
+            history.scrollRestoration = "manual";
+        }
+
+        window.scrollTo(0, 0);
+    });
+</script>
 
 <div class="min-h-screen bg-neutral-100 p-0 md:py-10 md:px-6 flex flex-col items-center">
     <div class="w-full max-w-md bg-white shadow-2xl rounded-none md:rounded-lg overflow-hidden">
-       	<Header/> 
-        <CountDown/>
-		<Venue/>
-        <MarriageList/>
-		<RSVP/>
-        <Footer/>
+        <Header />
+        <CountDown />
+        <Venue />
+        <MarriageList />
+        <RSVP />
+        <Footer />
     </div>
 </div>
